94e61e3c311c4c671ee7b5971650de434e2d23b3
Pankaj meena <rdjpankaj778269@gmail.com>
2026-01-15 14:50:42 +0530

    Day 2: Speech recognition with microphone

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ("hey my project started from day 1 which is 15-01-2026")
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said:", text)
+except sr.UnknownValueError:
+    print("Sorry, I could not understand.")
+except sr.RequestError:
+    print("Network error.")
